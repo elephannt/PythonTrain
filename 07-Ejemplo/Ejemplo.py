2278343126f732a7e5d8d813f3b5e0c405ac572e
elephannt <lopezvalmarco@gmail.com>
2017-03-02 16:50:34 -0800

    Practicas Automatas

---
+++ 07-Ejemplo/Ejemplo.py
@@ -0,0 +1,16 @@
+    #Programa 1
+
+#def mifuncion(Direccion,INE):
+#  Direccion_completo = Direccion,INE
+#  print Direccion_completo
+
+    #Programa 2
+
+# def saludar(Nombre,Mensaje='Hola'):
+#    print (Mensaje,Nombre)
+    ##En la funcion no debe haber espacios
+
+#saludar(Mensaje="Buen dia", Nombre="Karla")
+
+   #Programa 3
+
