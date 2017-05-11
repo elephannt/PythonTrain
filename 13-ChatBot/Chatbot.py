93ae2b5b0348298c960dcdaa48eb7da7a7db9950
elephannt <lopezvalmarco@gmail.com>
2017-05-11 16:34:03 -0700

    Patrones

--- 13-ChatBot/Chatbot.py
+++ 13-ChatBot/Chatbot.py
@@ -1,4 +1,18 @@
-import random, threading, time
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+
+import random, threading, time , sys , os
+from time import sleep
+
+##Builder
+##Es usado para permitir la creacion de una variedad de objetos complejos desde un objeto fuente (Producto),
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+
+## Abstract Factory // Factory Method
+##El patrón Abstract Factory nos permite crear, mediante una interfaz, conjuntos o familias de objetos
+##(denominados productos) que dependen mutuamuente y todo esto sin especificar cual es el objeto concreto.
 
 class MiThread(threading.Thread):
     def __init__(self,evento):
@@ -11,88 +25,139 @@ class MiThread(threading.Thread):
 evento = threading.Event()
 
 greetings = ['hola', 'hello', 'hi', 'Hi', 'hey!','hey']
+
+##Patron Prototype
+##tiene como finalidad crear nuevos objetos duplicándolos, clonando una instancia creada previamente.
+
 random_greeting = random.choice(greetings)
 
 question = ['How are you?','How are you doing?']
 responses = ['Okay',"I'm fine"]
 random_response = random.choice(responses)
 
+## Singleton
+##Es un patrón de diseño diseñado para restringir
+## la creación de objetos pertenecientes a una clase o el valor de un tipo a un único objeto.
+os.system('clear')
 print ("\n     +-------------------------------------------------------------------------------------------------+")
-user = raw_input("	 | Please enter your username: ")
-password = raw_input("	 | Please enter your password: ")
-print ("	 | Going to verify information, please wait...")
+print ("     |					         LOGIN SCREEN					       |")
+print ("     +-------------------------------------------------------------------------------------------------+")
+print "     | Current IP address: ",s.getsockname()[0]
+print ("\n     +-------------------------------------------------------------------------------------------------+")
+s.close()
+user = raw_input("     | Please enter your username: ")
+password = raw_input("     | Please enter your password: ")
+print ("     | Going to verify information, please wait...")
+print("\n")
+for i in range(21):
+    						sys.stdout.write('\r')
+    						sys.stdout.write("					[%-20s] %d%%" % ('='*i, 5*i))
+    						sys.stdout.flush()
+    						sleep(0.25)
 time.sleep(3)  # Es posible cambiar el numero
 evento.set()
+if user == "m" and password == "m":
+	os.system('clear')
+	s = """
+                      _-.                     .-_
+                  _..-'(                       )`-.._
+               ./'. '||\\.       (\_/)       .//||` .`\.
+            ./'.|'.'||||\\|..    )*.*(    ..|//||||`.`|.`\.
+        ./'..|'.|| |||||\```````  "  '''''''/||||| ||.`|..`\.
+      ./'.||'.|||| ||||||||||||.     .|||||||||||| ||||.`||.`\.
+     /'|||'.|||||| ||||||||||||{     }|||||||||||| ||||||.`|||`
+    '.|||'.||||||| ||||||||||||{     }|||||||||||| |||||||.`|||.`
+   '.||| ||||||||| |/'   ``\||/`     '\||/''   `\| ||||||||| |||.`
+   |/' \./'     `\./          |/\   /\|          \./'     `\./ `\|
+   V    V         V          }' `\ /' `{          V         V    V
+   `    `         `               U               '         '
 
-while True:
-	print ("     +-------------------------------------------------------------------------------------------------+")
-	print ("     |									    WELCOME BACK									  		   |")
-	print ("     +-------------------------------------------------------------------------------------------------+")
-	print ("     | CCON: Cannot connect on network						  								  		   |")
-	print ("     | ASO: All services out								  								  		   |")
-	print ("     | ACO: All channels out								  								  		   |")
-	print ("     | PHYS: Physical condition								  								  		   |")
-	print ("     +-------------------------------------------------------------------------------------------------+")
-
-	userInput = raw_input("     | Username >>> ")
-	if userInput in greetings:
-		print(random_greeting)
-	elif userInput in question:
-		print(random_response)
-
-	elif userInput == "CCON":
+"""
+	print (s)
+	print ("     	Loading program, please wait...\n")
+	print("")
+	for i in range(21):
+    						sys.stdout.write('\r')
+    						sys.stdout.write("     	[%-20s] %d%%" % ('='*i, 5*i))
+    						sys.stdout.flush()
+    						sleep(0.25)
+	while True:
+		os.system('clear')
+		print ("\n     +-------------------------------------------------------------------------------------------------+")
+		print ("     |					         WELCOME BACK					       |")
 		print ("     +-------------------------------------------------------------------------------------------------+")
-		print ("     | BestChatBOT: Do you have any lights on the BHR?									               |")
+		print ("     | CCON: Cannot connect on network						                       |")
+		print ("     | ASO: All services out								 	       |")
+		print ("     | ACO: All channels out								  	       |")
+		print ("     | PHYS: Physical condition						 			       |")
 		print ("     +-------------------------------------------------------------------------------------------------+")
-		userInput1 = raw_input("     | Username >>> ")
-		if userInput1 == "No" or userInput1 == "no":
+
+		userInput = raw_input("     | Username >>> ")
+		if userInput in greetings:
+			print(random_greeting)
+		elif userInput in question:
+			print(random_response)
+
+		elif userInput == "CCON":
 			print ("     +-------------------------------------------------------------------------------------------------+")
-			print ("     | BestChatBOT: Make sure that the BHR its connected to the poweroutlet. Do you have lights?	   |")
+			print ("     | BestChatBOT: Do you have any lights on the BHR?					               |")
 			print ("     +-------------------------------------------------------------------------------------------------+")
-			userInput2 = raw_input("     | Username >>> ")
-			if userInput2 == "Yes" or userInput2 == "yes":
+			userInput1 = raw_input("     | Username >>> ")
+			if userInput1 == "No" or userInput1 == "no":
 				print ("     +-------------------------------------------------------------------------------------------------+")
-				print ("     | BestChatBot: Do you have internet connection										  		       |")
+				print ("     | BestChatBOT: Make sure that the BHR its connected to the poweroutlet. Do you have lights?       |")
 				print ("     +-------------------------------------------------------------------------------------------------+")
-				userInput3 = raw_input("     | Username >>> ")
-				if userInput3 == "Yes" or userInput3 == "yes":
+				userInput2 = raw_input("     | Username >>> ")
+				if userInput2 == "Yes" or userInput2 == "yes":
 					print ("     +-------------------------------------------------------------------------------------------------+")
-					print ("     | BestChatBot: Its great to hear that we got your internet back, have a good day!			  	   |")
+					print ("     | BestChatBot: Do you have internet connection					               |")
 					print ("     +-------------------------------------------------------------------------------------------------+")
-					break
-				elif userInput3 == "No" or userInput3 == "no":
-					print ("     +-------------------------------------------------------------------------------------------------+")
-					print ("     | BestChatBot: We are going to Break the DHCP lease, please wait..								   |")
-					print ("     +-------------------------------------------------------------------------------------------------+")
-					time.sleep(3)  # Es posible cambiar el numero
-					evento.set()
-					print ("     +-------------------------------------------------------------------------------------------------+")
-					print ("     | BestChatBot: Do you have internet connection now?		 			                           |")
-					print ("     +-------------------------------------------------------------------------------------------------+")
-					userInput4 = raw_input("     | Username >>> ")
-					if userInput4 == "Yes" or userInput4 == "yes":
+					userInput3 = raw_input("     | Username >>> ")
+					if userInput3 == "Yes" or userInput3 == "yes":
 						print ("     +-------------------------------------------------------------------------------------------------+")
-						print ("     | BestChatBot: Its great to hear that we got your internet back , have a good day!			       |")
+						print ("     | BestChatBot: Its great to hear that we got your internet back, have a good day!			  	   |")
 						print ("     +-------------------------------------------------------------------------------------------------+")
 						break
-					elif userInput4 == "No" or userInput4 == "no":
+					elif userInput3 == "No" or userInput3 == "no":
 						print ("     +-------------------------------------------------------------------------------------------------+")
-						print ("     | BestChatBot: We will need a tech to check the connection, check for available techs, pls wait...|")
+						print ("     | BestChatBot: We are going to Break the DHCP lease, please wait..				       |")
+						print ("\n")
+						for i in range(21):
+    							sys.stdout.write('\r')
+    							sys.stdout.write("					[%-20s] %d%%" % ('='*i, 5*i))
+    							sys.stdout.flush()
+    							sleep(0.25)
+						print ("\n")
+						print ("\n     +-------------------------------------------------------------------------------------------------+")
 						print ("     +-------------------------------------------------------------------------------------------------+")
-						time.sleep(3)  # Es posible cambiar el numero
-						evento.set()
+						print ("     | BestChatBot: Do you have internet connection now?		 		               |")
 						print ("     +-------------------------------------------------------------------------------------------------+")
-						print ("     | Somebody will be showing today...have a good day.									           |")
-						print ("     +-------------------------------------------------------------------------------------------------+")
-						break
-		elif userInput1 == "Yes" or userInput1 == "yes":
-			print ("     +-------------------------------------------------------------------------------------------------+")
-			print ("     | Please check your internet connection...													       |")
-			print ("     +-------------------------------------------------------------------------------------------------+")
-
+						userInput4 = raw_input("     | Username >>> ")
+						if userInput4 == "Yes" or userInput4 == "yes":
+							print ("     +-------------------------------------------------------------------------------------------------+")
+							print ("     | BestChatBot: Its great to hear that we got your internet back, have a good day!")
+							print ("     +-------------------------------------------------------------------------------------------------+")
+							break
+						elif userInput4 == "No" or userInput4 == "no":
+							print ("     +-------------------------------------------------------------------------------------------------+")
+							print ("     | BestChatBot: We will need a tech to check the connection, check for available techs, pls wait...|")
+							print ("     +-------------------------------------------------------------------------------------------------+")
+							time.sleep(3)  # Es posible cambiar el numero
+							evento.set()
+							print ("     +-------------------------------------------------------------------------------------------------+")
+							print ("     | Somebody will be showing today...have a good day.		        		       |")
+							print ("     +-------------------------------------------------------------------------------------------------+")
+							break
+			elif userInput1 == "Yes" or userInput1 == "yes":
+				print ("     +-------------------------------------------------------------------------------------------------+")
+				print ("     | Please check your internet connection...							       |")
+				print ("     +-------------------------------------------------------------------------------------------------+")
+				break
+		elif userInput == "exit":
+			os.system('clear')
 			break
-	elif userInput == "exit":
-		break
-        else:
-            print("     | I did not understand what you said									|")
+        	else:
+            		print("     | I did not understand what you said							       |")
 
+else:
+	print ("\n     | Usuario incorrecto.\n")
